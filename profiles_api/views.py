4bfbe1fc8fb8e2f0b9b27e3f4cd8fa57506afa15
odytron <gizmapps@gmail.com>
2020-10-25 22:19:30 +0200

    change an error in views.py line 83

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -80,7 +80,7 @@ class HelloViewSet(viewsets.ViewSet):
 
         if serializer.is_valid():
             name = serializer.validated_data.get('name')
-            message = f'Hello {name}!'
+            message = {name}
             return Response({'message': message})
         else:
             return Response(
